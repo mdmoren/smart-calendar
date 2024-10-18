2797c815b5f83863de37264c502b2ac5fb71ff0b
mark moreno <markmoreno34@gmail.com>
2024-10-18 15:28:17 -0400

    clock set up

--- src/lib/components/Clock.svelte
+++ src/lib/components/Clock.svelte
@@ -1,7 +1,27 @@
 <script>
 	import StyledContainer from '$lib/components/StyledContainer.svelte';
+	import { onMount } from 'svelte';
+
+	let time;
+	const timeOptions = {
+		hour: '2-digit',
+		minute: '2-digit',
+		hour12: true
+	};
+
+	const updateTime = () => {
+		let newTime = new Date().toLocaleTimeString([], timeOptions);
+		time = newTime[0] === '0' ? newTime.slice(1) : newTime;
+	};
+
+	onMount(() => {
+		updateTime();
+		const interval = setInterval(updateTime, 1000);
+
+		return () => clearInterval(interval);
+	});
 </script>
 
 <StyledContainer height={'20%'}>
-	<div>Clock</div>
+	<p class="text-8xl font-semibold text-white pb-2">{time}</p>
 </StyledContainer>
