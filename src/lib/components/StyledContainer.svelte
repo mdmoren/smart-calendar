0aacc4c7a3c34a5b33d4f883bbfdd8c8be5384a3
mark moreno <markmoreno34@gmail.com>
2024-10-18 22:32:02 -0400

    add shadow to parent

--- src/lib/components/StyledContainer.svelte
+++ src/lib/components/StyledContainer.svelte
@@ -5,10 +5,10 @@
 	export let justify = 'center';
 </script>
 
-<div class="relative {addTailwind}" style="height: {height};">
+<div class="relative shadow-md {addTailwind}" style="height: {height};">
 	<div class="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-lg rounded-lg"></div>
 	<div
-		class="relative z-10 h-full w-full p-4 flex items-center rounded-lg shadow-md"
+		class="relative z-10 h-full w-full p-4 flex items-center rounded-lg"
 		style="flex-direction: {flexDirection}; justify-content: {justify};"
 	>
 		<slot />
