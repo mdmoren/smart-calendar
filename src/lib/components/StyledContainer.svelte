81b67940591e3ee29a8dea8500d5703122a06e37
mark moreno <markmoreno34@gmail.com>
2024-10-18 22:33:04 -0400

    nsm shadow looks dumb there

--- src/lib/components/StyledContainer.svelte
+++ src/lib/components/StyledContainer.svelte
@@ -5,7 +5,7 @@
 	export let justify = 'center';
 </script>
 
-<div class="relative shadow-md {addTailwind}" style="height: {height};">
+<div class="relative {addTailwind}" style="height: {height};">
 	<div class="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-lg rounded-lg"></div>
 	<div
 		class="relative z-10 h-full w-full p-4 flex items-center rounded-lg"
